55287b44caa78faf8856708a249baf47264e871a
ptucc327 <paultuccinardi@gmail.com>
2026-01-29 16:16:35 -0500

    feat: Created Readme File and improved model training

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ChurnLens
+
+Author: Paul Tuccinardi
+LinkedIn : [PaulTuccinardi](https://www.linkedin.com/in/paul-tuccinardi/)
+GitHub: [PTucc327](https://github.com/PTucc327)
+Email: <paultuccinardi@gmail.com>
+
--- src/model_training.py
+++ src/model_training.py
@@ -4,26 +4,34 @@ import shap
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score
-
+from imblearn.over_sampling import SMOTE
 
 def train_churn_model(data_path = 'data/featured_data.csv'):
     print("--- Starting Model Training Pipeline ---")
     df = pd.read_csv(data_path)
 
+
     # Define Feature Columns and Target Variable
     X = df.drop(columns=['customer_id', 'signup_date','churned' ])
     y = df['churned']
 
+
     # Split data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+
+    # Apply Smote to handle class imbalance
+    smote = SMOTE(random_state=42)
+    X_train, y_train = smote.fit_resample(X_train, y_train)
+    
+
     # Model Initialization and Training
-    model = xgb.XGBClassifier(use_label_encoder=False, 
+    model = xgb.XGBClassifier(
                               n_estimators=100,
                               learning_rate=0.1,
                               max_depth=5,
                               random_state=42,
-                              scale_pos_weight= (len(y_train)-sum(y_train)) /sum(y_train)
+                            #   scale_pos_weight= (len(y_train)-sum(y_train)) /sum(y_train)
                               )  # Handle class imbalance
     # Fit model
     model.fit(X_train, y_train)
@@ -37,6 +45,14 @@ def train_churn_model(data_path = 'data/featured_data.csv'):
     print("Confusion Matrix:")
     print(confusion_matrix(y_test, y_pred))
     print(f"ROC-AUC Score: {roc_auc_score(y_test, model.predict_proba(X_test)[:,1]):.4f}")
+
+
+    y_probs = model.predict_proba(X_test)[:, 1]
+    y_pred_new = (y_probs >= 0.2).astype(int)
+
+    print("--- Performance with 0.2 Threshold ---")
+    print(classification_report(y_test, y_pred_new))
+
 
     #SHAP Explainability
     explainer = shap.Explainer(model)
